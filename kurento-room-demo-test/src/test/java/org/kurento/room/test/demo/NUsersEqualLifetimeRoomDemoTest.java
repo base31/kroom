a77702b218e6aea4a8ccfdb041aec9e6873b55a6
Dejan Bundalo <dejan.bundalo@kpn.com>
2015-12-21 13:45:12 +0100

    Initial commit

---
+++ kurento-room-demo-test/src/test/java/org/kurento/room/test/demo/NUsersEqualLifetimeRoomDemoTest.java
@@ -0,0 +1,36 @@
+/*
+ * (C) Copyright 2015 Kurento (http://kurento.org/)
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License (LGPL)
+ * version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package org.kurento.room.test.demo;
+
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.kurento.room.test.NUsersEqualLifetime;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Room demo integration test (demo version).
+ * 
+ * @author <a href="mailto:rvlad@naevatec.com">Radu Tom Vlad</a>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@DemoTestConfig
+public class NUsersEqualLifetimeRoomDemoTest extends NUsersEqualLifetime {
+
+	@BeforeClass
+	public static void setupBeforeClass() {
+		appUrl = DEMO_ROOM_APP_URL;
+	}
+
+}
